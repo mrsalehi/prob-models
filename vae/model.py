d811ead8cec099e1c72a741e96bdeb2db860988f
mrsalehi <salehi.mohammadreza1997@gmail.com>
2021-02-28 11:47:05 -0500

    Initial commit on vae

---
+++ vae/model.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+
+
+class encoder(nn.Module):
+    pass
+
+class Decoder(nn.Module):
+    def __init__(self, n_layers, dim_in):
+
+class VAE(nn.Module):
+    def __init__(self, n_layers, dim_in, dim_out):
+        
+
+    def forward(x):
+        pass
+
+
